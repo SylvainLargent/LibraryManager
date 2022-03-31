48d11156689df65e61182c97c2adb5c1c3dac085
Sylvain Largent <sylvain.larg@hotmail.com>
2022-03-31 23:06:39 +0200

    README

---
+++ README.m
@@ -0,0 +1,3 @@
+Le site est bien fonctionelle, mais la moitié des icones ne sont pas visibles.
+L'encodage du site en UTF-8 ne semble pas fonctionner, malgré le charset = utf8.
+Le site est "joli" sur Google Chrome mais pas Firefox.
